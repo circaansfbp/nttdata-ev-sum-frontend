ea402f8a87d0f948aad65bfe91a5e2a20d3dcda2
Francisca Bustos Pendola <fbustosp@emeal.nttdata.com>
2022-02-16 17:09:53 -0300

    modificaciones de formato

--- src/app/service/carrito/carritoapi.service.ts
+++ src/app/service/carrito/carritoapi.service.ts
@@ -10,52 +10,52 @@ import { Producto } from 'src/app/class/producto';
 export class CarritoapiService {
 
   carrito = new Carrito();
-  productList= new BehaviorSubject<any>([]); 
-  constructor(private http:HttpClient) { }
+  productList = new BehaviorSubject<any>([]);
+  constructor(private http: HttpClient) { }
 
-  getProductData(){
+  getProductData() {
     return this.productList.asObservable();
   }
 
-  setProduct(producto:any){
+  setProduct(producto: any) {
     this.carrito.productos.push(...producto);
     this.productList.next(producto);
   }
 
-  addToCart(producto:any, cant:number){
-    const productoEnCarrito = this.carrito.productos.find((p:any)=>p.id===producto.id)
-    if(productoEnCarrito && productoEnCarrito.cantidad){
+  addToCart(producto: any, cant: number) {
+    const productoEnCarrito = this.carrito.productos.find((p: any) => p.id === producto.id)
+    if (productoEnCarrito && productoEnCarrito.cantidad) {
       productoEnCarrito.cantidad += cant;
 
-    }else{
-      producto.cantidad =cant;
+    } else {
+      producto.cantidad = cant;
       this.carrito.productos.push(producto);
-      
+
     }
     this.productList.next(this.carrito);
     this.getTotal();
   }
 
-  getTotal(){
-    let grandTotal=0;
-    this.carrito.productos.forEach((a:any)=>{
-      grandTotal +=(a.precio * a.cantidad);
+  getTotal() {
+    let grandTotal = 0;
+    this.carrito.productos.forEach((a: any) => {
+      grandTotal += (a.precio * a.cantidad);
     })
     this.carrito.precioTotalProducto = grandTotal;
   }
 
-  removeCarritoProduct(producto:Producto){
-    const index= this.carrito.productos.findIndex((p: any)=> p.id===producto.id)
-    this.carrito.productos.splice(index,1)
+  removeCarritoProduct(producto: Producto) {
+    const index = this.carrito.productos.findIndex((p: any) => p.id === producto.id)
+    this.carrito.productos.splice(index, 1)
     this.productList.next(this.carrito)
   }
 
-  removeCarrito(){
-    this.carrito.productos=[]
+  removeCarrito() {
+    this.carrito.productos = []
     this.productList.next(this.carrito)
   }
 
-  guardarCarrito(){
+  guardarCarrito() {
     console.log(this.carrito);
   }
 }
