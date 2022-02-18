c6dfbd2a5289582894d3d7df0441f3ba44e02c52
carolinapereira96 <carolina.pereira1601@alumnos.ubiobio.cl>
2022-02-18 14:07:54 -0300

    Filtros fix

--- src/app/pipes/filtro-categoria.pipe.ts
+++ src/app/pipes/filtro-categoria.pipe.ts
@@ -6,14 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FiltroCategoriaPipe implements PipeTransform {
 
   transform(value: any, arg: any): any {
-    // const resultadoProductos = [];
-    // for (const producto of value) {
-    //   if (producto.categoria.toLowerCase().(arg.toLowerCase()) > -1) {
-    //     resultadoProductos.push(producto);
+    const resultadoProductos = [];
+    console.log(arg);
+    for (const producto of value) {
+      if (producto.categoria.toLowerCase().indexOf(arg.toLowerCase() > -1)) {
+        resultadoProductos.push(producto);
 
-    //   };
-    // };
-    // return resultadoProductos;
+      };
+    };
+    return resultadoProductos;
   }
 
 }
